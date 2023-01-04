c65adf2f74a029e792200a6851f8654dccc2dfe7
raoji patil <raojipatil22@gmail.com>
2023-01-04 21:50:37 +0530

    array

--- Array/array.js
+++ Array/array.js
@@ -12,3 +12,39 @@ for (var i = 0; i < arr.length; i++) {
     }
 }
 console.log(sum.join(" "));
+
+
+//Q.2  Palindrome Number
+
+var num = 129;
+var out = '';
+for(var i = num.length; i >= 0; i--) {
+   out = out + num[i];
+   console.log(out);
+}
+
+
+var x = 121;
+var a =x.toString().split('').reverse().join("");
+
+if(x == a) {
+console.log("true");
+} else {
+  console.log("false");
+}
+
+
+
+//Q 3; Search Numbers
+
+var arr = [10, 1, 10, 3, 4];
+var q = [2, 9];
+var count = 0; 
+
+
+for (var i = 0; i < arr.length; i++) {
+    if(arr[i] >= q[0] && arr[i] <= q[1]){
+        count++;
+    }
+}
+console.log(count);
